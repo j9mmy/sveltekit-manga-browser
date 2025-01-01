b5d9c93ecb7a400310ae9b5138c1c63393f873c6
j9mmy <jimmy.tukiran@outlook.com>
2025-01-01 03:13:01 +0100

    feat: profile (view your manga list)
    overview of user profile and your personal manga list

---
+++ src/routes/profile/+page.server.ts
@@ -0,0 +1,23 @@
+import { db } from "$lib/server/db";
+import { users, users_manga } from "$lib/server/db/schema";
+import { error } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+import { eq } from "drizzle-orm";
+
+export const load = (async () => {
+  const [user] = await db.select().from(users).limit(1);
+  if (!user) error(404, { message: "User was not found" });
+  console.log(user);
+
+  const listEntries = await db
+    .select()
+    .from(users_manga)
+    .where(eq(users_manga.userId, user.id));
+  if (!listEntries) error(404, { message: "List entries were not found" });
+  console.log(listEntries);
+
+  return {
+    listEntries,
+    user,
+  };
+}) satisfies PageServerLoad;
---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.ts";
+  import type { PageData } from "./$types";
+
+  let { data }: { data: PageData } = $props();
+</script>
+
+<div class="grid gap-4 text-sm">
+  <div class="flex justify-between items-end gap-4 mb-2">
+    <h1 class=" leading-none">Profile</h1>
+    <div class="flex gap-4 text-end">
+      <strong>{data.user.name}</strong>
+      —
+      <p>Age: <strong>{data.user.age}</strong></p>
+    </div>
+  </div>
+
+  <hr />
+
+  <h3>Your list</h3>
+  <div class="grid gap-2">
+    {#if data.listEntries.length}
+      <div
+        class="grid grid-cols-8 items-center gap-x-4 gap-y-1 text-muted-foreground"
+      >
+        <p class="mb-2 text-center">Manga ID</p>
+        <p class="mb-2 col-span-4">Title</p>
+        <p class="mb-2 col-span-2">Status</p>
+        <p class="mb-2">Score</p>
+        {#each data.listEntries as entry}
+          <Button
+            href="/manga/{entry.mangaId}"
+            variant="outline"
+            size="sm"
+            class="p-0 grid grid-cols-8 col-span-8 gap-x-4 text-primary"
+            data-sveltekit-preload-data="tap"
+          >
+            <p class="text-center">{entry.mangaId}</p>
+            <strong class="col-span-4">{entry.title}</strong>
+            <p class="col-span-2 capitalize">{entry.status ?? "None"}</p>
+            <p>{entry.score ?? "None"}</p>
+          </Button>
+        {/each}
+      </div>
+    {:else}
+      <p class="text-muted-foreground">No entries found</p>
+    {/if}
+  </div>
+</div>
